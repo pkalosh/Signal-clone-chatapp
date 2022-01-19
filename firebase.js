93964d16ff13ac4b9457e53f897b70439f5c1c27
patrick <patokalosh@gmail.com>
2022-01-19 08:29:52 +0300

    chat app in react native

--- firebase.js
+++ firebase.js
@@ -7,12 +7,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCuTJAh71xDlUx0E8FJMXQv8AYtrGU9CM8",
-    authDomain: "signal-app-clone-8f7d6.firebaseapp.com",
-    projectId: "signal-app-clone-8f7d6",
-    storageBucket: "signal-app-clone-8f7d6.appspot.com",
-    messagingSenderId: "839131391714",
-    appId: "1:839131391714:web:de6dcbd6f06bd3d9f120bb"
+  firebase  config
   };
 
   let app;
